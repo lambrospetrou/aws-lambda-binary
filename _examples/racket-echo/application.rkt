735c3eb0c477282c01d1dd9abb667f61e26b47ad
Lambros Petrou <lambrospetrou@gmail.com>
2018-03-26 22:05:15 +0100

    Fix the Racket example to flush stdout. Fixes #2

--- _examples/racket-echo/application.rkt
+++ _examples/racket-echo/application.rkt
@@ -2,7 +2,8 @@
 
 ;; This is the actual logic of our code!
 (define (execute-logic data)
-  (display (format "data: ~a~%" data)))
+  (display (format "data: ~a~%" data))
+  (flush-output))
 
 ;; The following code waits for one line of input and then dispatches it to the `execute-logic` function.
 ;; This way we can have full control over what we can do and there can be an arbitrarily complex protocol
